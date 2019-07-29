3127eb7c8dde59c2835ef98a83b1f4afdf6f07d7
Gantumur <tsgantumur@gmail.com>
2019-07-29 01:09:44 -0500

    lab3 Proxy

---
+++ src/lab3/proxy/IRow.java
@@ -0,0 +1,6 @@
+package lab3.proxy;
+
+public interface IRow {
+	public int getNum();
+	public void setNum(int num);
+}
---
+++ src/lab3/proxy/Row.java
@@ -0,0 +1,23 @@
+package lab3.proxy;
+
+public class Row implements IRow {
+	
+	private int num;
+
+	public Row()
+	{
+	}
+
+	@Override
+	public int getNum() {
+		// TODO Auto-generated method stub
+		return num;
+	}
+	
+	@Override
+	public void setNum(int num)
+	{
+		this.num = num;
+	}
+
+}
